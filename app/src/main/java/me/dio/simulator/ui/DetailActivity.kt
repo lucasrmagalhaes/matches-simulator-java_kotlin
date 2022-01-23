116cc622bfa668760e39353a7d940b060e9e536e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-23 09:14:25 -0300

    Finalização do App

--- app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
@@ -32,6 +32,22 @@ class DetailActivity : AppCompatActivity() {
             supportActionBar?.title = it.place.name
 
             binding.tvDescription.text = it.description
+
+            Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+
+            if (it.homeTeam.score != null) {
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+
+            Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+
+            if (it.awayTeam.score != null) {
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
         }
     }
 }
