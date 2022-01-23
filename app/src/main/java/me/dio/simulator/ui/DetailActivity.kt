67dd8750a70b1b330306d7239e4663911ffde71f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-23 08:54:27 -0300

    Carregando a tela e botão voltar funcionando

--- app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
@@ -6,6 +6,10 @@ import me.dio.simulator.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
+    object Extras {
+        const val MATCH = "EXTRA_MATCH"
+    }
+
     private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/me/dio/simulator/ui/adapter/MatchesAdapter.java
+++ app/src/main/java/me/dio/simulator/ui/adapter/MatchesAdapter.java
@@ -1,6 +1,7 @@
 package me.dio.simulator.ui.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 import me.dio.simulator.databinding.MatchItemBinding;
 import me.dio.simulator.domain.Match;
+import me.dio.simulator.ui.DetailActivity;
 
 public class MatchesAdapter extends RecyclerView.Adapter<MatchesAdapter.ViewHolder> {
 
@@ -53,6 +55,12 @@ public class MatchesAdapter extends RecyclerView.Adapter<MatchesAdapter.ViewHold
         if (match.getAwayTeam().getScore() != null) {
             holder.binding.tvAwayTeamScore.setText(String.valueOf(match.getAwayTeam().getScore()));
         }
+
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra(DetailActivity.Extras.MATCH, match);
+            context.startActivity(intent);
+        });
     }
 
     @Override
